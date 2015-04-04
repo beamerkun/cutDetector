6ce55710fa5bdebeed982dc95fc6116c129fa24a
Jakub 'beamerkun' Sokół <beamer.kun@gmail.com>
2015-04-04 21:16:39 +0200

    Added opencv image widget.

---
+++ gui/opencv_image_widget.h
@@ -0,0 +1,31 @@
+#ifndef OPENCV_IMAGE_WIDGET_H
+#define OPENCV_IMAGE_WIDGET_H
+
+// Source:
+// http://develnoter.blogspot.com/2012/05/integrating-opencv-in-qt-gui.html
+
+#include <QObject>
+#include <QWidget>
+
+#include <opencv2/opencv.hpp>
+
+class OpenCVImageWidget : public QWidget {
+  Q_OBJECT
+ public:
+  explicit OpenCVImageWidget(QWidget* parent = 0);
+  ~OpenCVImageWidget();
+
+  QSize sizeHint() const { return _qimage.size(); }
+  QSize minimumSizeHint() const { return _qimage.size(); }
+
+ public slots:
+  void showImage(const cv::Mat& image);
+
+ protected:
+  void paintEvent(QPaintEvent* event);
+
+  QImage _qimage;
+  cv::Mat _tmp;
+};
+
+#endif  // OPENCV_IMAGE_WIDGET_H
