58e4a046e5afc30679dd43a2e2261765ecf834e7
Jakub 'beamerkun' Sokół <beamer.kun@gmail.com>
2015-06-18 21:39:37 +0200

    Clean up gui initialization.
    Remove now obsolete gui.hpp/cpp

--- gui/main_gui.cpp
+++ gui/main_gui.cpp
@@ -1,8 +1,15 @@
+#include <memory>
+
+#include <QApplication>
+
 #include <cut_detector.hpp>
-#include <gui.hpp>
+
+#include <main_window.h>
 
 int main(int argc, char* argv[]) {
-  std::unique_ptr<CutDetector> detector(new CutDetector());
-  gui::startGraphicsInterface(argc, argv, detector.release());
+  std::unique_ptr<QApplication> app(new QApplication(argc, argv));
+  main_window main(nullptr, new CutDetector());
+  main.show();
+  app->exec();
   return 0;
 }
