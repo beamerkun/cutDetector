a12df4d653fe1a60b685256448cbea3e72a44d62
Jakub 'beamerkun' Sokół <beamer.kun@gmail.com>
2016-04-16 14:43:39 +0200

    Testing WIP #3
    - improve test naming
    - introduce test for wait time on cut found

--- tests/scene_detector_test.cpp
+++ tests/scene_detector_test.cpp
@@ -1,6 +1,13 @@
 #include "gmock/gmock.h"
 
+#include <iostream>
+#include <random>
 #include <memory>
+#ifdef WIN32
+#include <windows.h> // GetTickCount
+#else
+#include <sys/time.h> // timeval, gettimeofday
+#endif  // win32
 
 #include "scene_detector.h"
 
@@ -70,7 +77,7 @@ TYPED_TEST(SceneDetectorTest, willUseEveryFrame) {
   this->detector_->detectScenes(&this->reader_mock_, &this->comparator_mock_);
 }
 
-TYPED_TEST(SceneDetectorTest, noCuts) {
+TYPED_TEST(SceneDetectorTest, canDetectZeroCuts) {
   EXPECT_CALL(this->reader_mock_, isOpen());
   EXPECT_CALL(this->reader_mock_, getTotalFrameCount());
   EXPECT_CALL(this->reader_mock_, getFrame(_, _))
@@ -90,7 +97,7 @@ TYPED_TEST(SceneDetectorTest, noCuts) {
   EXPECT_EQ(result[0], std::make_pair(0, this->frame_count_ - 1));
 }
 
-TYPED_TEST(SceneDetectorTest, oneCut) {
+TYPED_TEST(SceneDetectorTest, canDetectOneCut) {
   EXPECT_CALL(this->reader_mock_, isOpen());
   EXPECT_CALL(this->reader_mock_, getTotalFrameCount());
   EXPECT_CALL(this->reader_mock_, getFrame(_, _))
@@ -111,4 +118,68 @@ TYPED_TEST(SceneDetectorTest, oneCut) {
   EXPECT_EQ(result.size(), (size_t)2);
   EXPECT_EQ(result[0].first, 0);
   EXPECT_EQ(result[1].second, this->frame_count_ - 1);
+}
+
+int getTimeMsecs() {
+#ifdef WIN32
+      return GetTickCount();
+#else
+    timeval now;
+    gettimeofday(&now, 0);
+    return now.tv_sec * 1000 + now.tv_usec / 1000;
+#endif // win32
+}
+
+TYPED_TEST(SceneDetectorTest, isWaitTimeWorking) {
+  EXPECT_CALL(this->reader_mock_, isOpen());
+  EXPECT_CALL(this->reader_mock_, getTotalFrameCount());
+  EXPECT_CALL(this->reader_mock_, getFrame(_, _))
+      .Times(AtLeast(this->frame_count_));
+
+  EXPECT_CALL(this->comparator_mock_, isDifferentScene(_, _, _))
+      .WillOnce(Return(false))
+      .WillOnce(Return(true))
+      .WillRepeatedly(Return(false));
+
+  EXPECT_CALL(this->observer_mock_, OnDifferenceCalculated(_, _, _))
+      .Times(AtLeast(1));
+  EXPECT_CALL(this->observer_mock_, OnCutDetected(_, _)).Times(1);
+
+  int wait_time = 50;
+  this->detector_->setWaitTime(wait_time);
+
+  int start = getTimeMsecs();
+  this->detector_->detectScenes(&this->reader_mock_, &this->comparator_mock_);
+  int end = getTimeMsecs();
+
+  EXPECT_GE(end - start, wait_time);
+}
+
+TYPED_TEST(SceneDetectorTest, isRandomWaitTimeWorking) {
+  EXPECT_CALL(this->reader_mock_, isOpen());
+  EXPECT_CALL(this->reader_mock_, getTotalFrameCount());
+  EXPECT_CALL(this->reader_mock_, getFrame(_, _))
+      .Times(AtLeast(this->frame_count_));
+
+  EXPECT_CALL(this->comparator_mock_, isDifferentScene(_, _, _))
+      .WillOnce(Return(false))
+      .WillOnce(Return(true))
+      .WillRepeatedly(Return(false));
+
+  EXPECT_CALL(this->observer_mock_, OnDifferenceCalculated(_, _, _))
+      .Times(AtLeast(1));
+  EXPECT_CALL(this->observer_mock_, OnCutDetected(_, _)).Times(1);
+
+  std::mt19937 rng;
+  int seed = getTimeMsecs();
+  rng.seed(seed);
+  std::cout << "               Random seed = " << seed << std::endl;
+  int wait_time = rng() % 100;
+  this->detector_->setWaitTime(wait_time);
+
+  int start = getTimeMsecs();
+  this->detector_->detectScenes(&this->reader_mock_, &this->comparator_mock_);
+  int end = getTimeMsecs();
+
+  EXPECT_GE(end - start, wait_time);
 }
